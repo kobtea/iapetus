19154d349b69f72febfceeb6d4d50fb87226cf31
Hiroaki Kobayashi <kobtea9696@gmail.com>
2018-05-05 16:18:07 +0900

    support shordhands for time config

--- pkg/util/time.go
+++ pkg/util/time.go
@@ -18,11 +18,27 @@ import (
 	"math"
 	"strconv"
 	"time"
+	"strings"
+	"github.com/prometheus/common/model"
 )
 
-// origin: https://github.com/prometheus/prometheus/blob/v2.2.1/web/api/v1/api.go#L798
-// modify: make the scope of `parseTime` to public.
 func ParseTime(s string) (time.Time, error) {
+	if t, err := parseTime(s); err == nil {
+		return t, nil
+	}
+	if elms := strings.Split(s, "now-"); len(elms) == 2 {
+		// expect duration
+		d, err := ParseDuration(elms[1])
+		if err != nil {
+			return time.Time{}, fmt.Errorf("cannot parse %q to a valid duration", elms[1])
+		}
+		return time.Now().Add(-d), nil
+	}
+	return time.Time{}, fmt.Errorf("cannot parse %q to a valid timestamp", s)
+}
+
+// origin: https://github.com/prometheus/prometheus/blob/v2.2.1/web/api/v1/api.go#L798
+func parseTime(s string) (time.Time, error) {
 	if t, err := strconv.ParseFloat(s, 64); err == nil {
 		s, ns := math.Modf(t)
 		return time.Unix(int64(s), int64(ns*float64(time.Second))), nil
@@ -31,4 +47,20 @@ func ParseTime(s string) (time.Time, error) {
 		return t, nil
 	}
 	return time.Time{}, fmt.Errorf("cannot parse %q to a valid timestamp", s)
+}
+
+// origin: https://github.com/prometheus/prometheus/blob/v2.2.1/web/api/v1/api.go#L809
+// modify: make the scope of `parseDuration` to public.
+func ParseDuration(s string) (time.Duration, error) {
+	if d, err := strconv.ParseFloat(s, 64); err == nil {
+		ts := d * float64(time.Second)
+		if ts > float64(math.MaxInt64) || ts < float64(math.MinInt64) {
+			return 0, fmt.Errorf("cannot parse %q to a valid duration. It overflows int64", s)
+		}
+		return time.Duration(ts), nil
+	}
+	if d, err := model.ParseDuration(s); err == nil {
+		return time.Duration(d), nil
+	}
+	return 0, fmt.Errorf("cannot parse %q to a valid duration", s)
 }
