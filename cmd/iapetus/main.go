5327fb8e0ab93188d49f669cd5414446c51d572f
Hiroaki Kobayashi <kobtea9696@gmail.com>
2018-05-13 22:49:38 +0900

    make cli

---
+++ cmd/iapetus/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kobtea/iapetus/pkg/config"
+	"github.com/kobtea/iapetus/pkg/proxy"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"io/ioutil"
+	"net/http"
+)
+
+var (
+	configFile = kingpin.Flag("config", "iapetus config file path.").Required().String()
+	addr       = kingpin.Flag("addr", "address to listen.").Default(":19090").String()
+)
+
+func main() {
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
+
+	buf, err := ioutil.ReadFile(*configFile)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	c, err := config.Parse(buf)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	handler, err := proxy.NewProxyHandler(*c)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	server := http.Server{
+		Addr:    *addr,
+		Handler: handler,
+	}
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println(err.Error())
+	}
+}
